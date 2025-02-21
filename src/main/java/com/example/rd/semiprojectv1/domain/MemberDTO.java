0bd73a96194e12c84c5fdb39fe7dac6ecd14f931
min <fheka77@gmail.com>
2025-02-21 11:50:53 +0900

    MemberRepository 작성 MemberDTO builder 패턴 추가 MemberRepository 테스트 작성

--- src/main/java/com/example/rd/semiprojectv1/domain/MemberDTO.java
+++ src/main/java/com/example/rd/semiprojectv1/domain/MemberDTO.java
@@ -1,8 +1,10 @@
 package com.example.rd.semiprojectv1.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data//setter , getter , tostring 자동 생성
+@Builder//lombok 에 있음.
 public class MemberDTO {
     private String userid;
     private String passwd;
