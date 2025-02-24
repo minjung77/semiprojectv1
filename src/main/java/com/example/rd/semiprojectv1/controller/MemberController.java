0b471d53048f772c95a5d444df51af97a68b639e
min <fheka77@gmail.com>
2025-02-24 11:42:28 +0900

    fetch를 이용한 회원가입 기능 구현 2 - 컨트롤러에서 ResponseEntity 사용

--- src/main/java/com/example/rd/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/rd/semiprojectv1/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.example.rd.semiprojectv1.repository.MemberRepository;
 import com.example.rd.semiprojectv1.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,15 +23,17 @@ public class MemberController {
     public String join() {
         return "views/member/join";
     }
+
+    // ResponseEntity 는 스프링에서 HTTP와 관련된 기능을 구현할 때 사용.
+    // 상태코드, HTTP 헤더, HTTP 본문 등을 명시적으로 설정 가능
     @PostMapping("/join")
-    public String joinok(MemberDTO member) {
-        String returnPage = "redirect:/error";
-        log.info("submit된 회원 정보 : {}", member);
+    public ResponseEntity<?> joinok(MemberDTO member) {
+        ResponseEntity<?> response = ResponseEntity.badRequest().build();
 
         if(memberService.newMember(member))
-            returnPage = "redirect:/member/login";
+            response = ResponseEntity.ok().build();
 
-        return returnPage;
+        return response;
     }
     @GetMapping("/login")
     public String login() {
