fc8b937953a610cef0fae230556afb5c11002a36
min <fheka77@gmail.com>
2025-04-01 09:18:07 +0900

    Jwt 인증 필터, Jwt 토큰 유틸 구현, Jwt 인증 설정

--- src/main/java/com/example/rd/semiprojectv1/config/SecurityConfig.java
+++ src/main/java/com/example/rd/semiprojectv1/config/SecurityConfig.java
@@ -2,57 +2,77 @@ package com.example.rd.semiprojectv1.config;
 
 import com.example.rd.semiprojectv1.custom.CustomAuthenticationFailureHandler;
 import com.example.rd.semiprojectv1.custom.CustomAuthenticationSuccessHandler;
+import com.example.rd.semiprojectv1.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-    private final UserDetailsService userDetailService;
-    private final CustomAuthenticationSuccessHandler successHandler;
-    private final CustomAuthenticationFailureHandler failureHandler;
+//    private final UserDetailsService userDetailService;
+//    private final CustomAuthenticationSuccessHandler successHandler;
+//    private final CustomAuthenticationFailureHandler failureHandler;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 //                .csrf().disable()//CSRF 필터를 끔
-                    .userDetailsService(userDetailService)
+//                    .userDetailsService(userDetailService)
                     .authorizeRequests()//url 기반 인가 설정
                     .antMatchers("/member/logout", "member/myinfo", "/board/write", "/gallery/write", "board/reply").authenticated()// 인증 받은 사용자감 접근 가능
                     .antMatchers("/**", "/member/**", "/gallery/**", "/board/**").permitAll()// 인증/인가 여부와 상관없이 접근 가능
                     .antMatchers("/css/**", "/js/**", "/image/**", "/favicon.ico/**").permitAll()// 인증/인가 여부와 상관없이 접근 가능
                 .and()
-                .formLogin()
-                    .loginPage("/member/login") //커스텀 로그인 페이지 설정
-                    .usernameParameter("userid") //아이디 매개변수 지정 !!
-                    .passwordParameter("passwd") //비밀번호 매개변수 지정 !!
-                    .defaultSuccessUrl("/member/myinfo")//로그인 성공 시 리다이텍트 url
-                    .failureUrl("/member/loginfail")//로그인 실패시 리다이렉트 url
-                    .successHandler(successHandler)
-                    .failureHandler(failureHandler)
-                    .permitAll()
-                .and()
-                .logout()// 로그아웃 설정
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                    .logoutSuccessUrl("/")// 로그아웃 성공 후 리다이렉트될 url
-                    .invalidateHttpSession(true)// 세션 무효화
-                    .deleteCookies("JSESSIONED")//JSESSIONID 쿠키 삭제
-                .permitAll();
+//                .formLogin()
+//                    .loginPage("/member/login") //커스텀 로그인 페이지 설정
+//                    .usernameParameter("userid") //아이디 매개변수 지정 !!
+//                    .passwordParameter("passwd") //비밀번호 매개변수 지정 !!
+//                    .defaultSuccessUrl("/member/myinfo")//로그인 성공 시 리다이텍트 url
+//                    .failureUrl("/member/loginfail")//로그인 실패시 리다이렉트 url
+//                    .successHandler(successHandler)
+//                    .failureHandler(failureHandler)
+//                    .permitAll()
+//                .and()
+//                .logout()// 로그아웃 설정
+//                    .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+//                    .logoutSuccessUrl("/")// 로그아웃 성공 후 리다이렉트될 url
+//                    .invalidateHttpSession(true)// 세션 무효화
+//                    .deleteCookies("JSESSIONED")//JSESSIONID 쿠키 삭제
+//                .permitAll();
+
+                //jwt 인증으로 변경
+                .sessionManagement()//jwt인증을 위해 stateless 설정
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //jwt 필터 설정
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
